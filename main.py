a39cfae2839f8704e22e2e0ca849ea05c915b0e3
SwanandPurkar <87146103+SwanandPurkar@users.noreply.github.com>
2024-05-04 11:20:43 +0530

    files uploaded

---
+++ main.py
@@ -0,0 +1,124 @@
+from collections import deque
+import tensorflow as tf
+from moviepy.editor import *
+import numpy as np
+import cv2
+import streamlit as st
+import base64
+
+IMAGE_HEIGHT = 64
+IMAGE_WIDTH = 64
+SEQUENCE_LENGTH = 20
+CLASSES_LIST = ["ApplyEyeMakeup", "ApplyLisptick", "Archery", "Basketball", "BenchPress", "Biking", "Bowling", "BrushingTeeth", "CricketShot", "Diving"]
+LRCN_model = tf.keras.models.load_model('./model.h5')
+
+def predict_on_video(video_file_path, output_file_path, SEQUENCE_LENGTH):
+    '''
+    This function will perform action recognition on a video using the LRCN model.
+    Args:
+    video_file_path:  The path of the video stored in the disk on which the action recognition is to be performed.
+    output_file_path: The path where the ouput video with the predicted action being performed overlayed will be stored.
+    SEQUENCE_LENGTH:  The fixed number of frames of a video that can be passed to the model as one sequence.
+    '''
+
+    # Initialize the VideoCapture object to read from the video file.
+    video_reader = cv2.VideoCapture(video_file_path)
+
+    # Get the width and height of the video.
+    original_video_width = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
+    original_video_height = int(video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # Initialize the VideoWriter Object to store the output video in the disk.
+    video_writer = cv2.VideoWriter(output_file_path, cv2.VideoWriter_fourcc('M', 'P', '4', 'V'),
+                                   video_reader.get(cv2.CAP_PROP_FPS), (original_video_width, original_video_height))
+
+    # Declare a queue to store video frames.
+    frames_queue = deque(maxlen = SEQUENCE_LENGTH)
+
+    # Initialize a variable to store the predicted action being performed in the video.
+    predicted_class_name = ''
+
+    # Iterate until the video is accessed successfully.
+    while video_reader.isOpened():
+
+        # Read the frame.
+        ok, frame = video_reader.read()
+
+        # Check if frame is not read properly then break the loop.
+        if not ok:
+            break
+
+        # Resize the Frame to fixed Dimensions.
+        resized_frame = cv2.resize(frame, (IMAGE_HEIGHT, IMAGE_WIDTH))
+
+        # Normalize the resized frame by dividing it with 255 so that each pixel value then lies between 0 and 1.
+        normalized_frame = resized_frame / 255
+
+        # Appending the pre-processed frame into the frames list.
+        frames_queue.append(normalized_frame)
+
+        # Check if the number of frames in the queue are equal to the fixed sequence length.
+        if len(frames_queue) == SEQUENCE_LENGTH:
+
+            # Pass the normalized frames to the model and get the predicted probabilities.
+            predicted_labels_probabilities = LRCN_model.predict(np.expand_dims(frames_queue, axis = 0))[0]
+
+            # Get the index of class with highest probability.
+            predicted_label = np.argmax(predicted_labels_probabilities)
+
+            # Get the class name using the retrieved index.
+            predicted_class_name = CLASSES_LIST[predicted_label]
+
+        # Write predicted class name on top of the frame.
+        cv2.putText(frame, predicted_class_name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        # Write The frame into the disk using the VideoWriter Object.
+        video_writer.write(frame)
+
+    # Release the VideoCapture and VideoWriter objects.
+    video_reader.release()
+    video_writer.release()
+
+
+
+
+def main():
+    st.title("Video Processing App")
+
+    # File Upload
+    st.header("Upload .avi File")
+    uploaded_file = st.file_uploader("Choose a .avi file", type="avi")
+
+    if uploaded_file is not None:
+        # st.video(uploaded_file.read(), "video/avi")
+
+        with open("input.avi", "wb") as f:
+            f.write(uploaded_file.read())
+
+        # Button to Generate Prediction
+        if st.button("Generate Prediction"):
+            with st.spinner("Generating Prediction..."):
+                output_file_path = "output.mp4"  # Output file path
+                predict_on_video("./input.avi", output_file_path, SEQUENCE_LENGTH)  # Call predict function
+
+            st.success("Prediction generated successfully!")
+
+            # with open("./output.mp4", "rb") as f:
+            #     st.video(f.read())
+
+            # Download Link for Prediction
+            st.header("Download Predicted Video")
+            st.markdown(get_download_link(output_file_path), unsafe_allow_html=True)
+
+
+
+
+def get_download_link(file_path):
+    with open(file_path, "rb") as file:
+        data = file.read()
+    b64 = base64.b64encode(data).decode('utf-8')
+    href = f'<a href="data:file/mp4;base64,{b64}" download="output.mp4">Download .mp4 file</a>'
+    return href
+
+if __name__ == "__main__":
+    main()
